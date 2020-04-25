5d2b383b67af82a83350c7264728e68b02d23aa1
chris <64315364+56157129@users.noreply.github.com>
2020-04-25 20:26:56 +0700

    Create evn.js

---
+++ evn.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'Go2Free',
+  // 站点描述
+  description: '您离自由只差一步~',
+  // API
+  host: 'https://api.init6.xyz',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'https://www.bing.com/th?id=OHR.FalklandRockhoppers_ROW9057491810_1920x1080.jpg&rf=LaDigue_1920x1080.jpg'
+}
